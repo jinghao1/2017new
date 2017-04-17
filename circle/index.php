95351bdcf1b745adae45acfe4d4e7587c540106e
songjinghao2012@163.com <songjinghao2012@163.com>
2017-04-17 14:34:49 +0800

    r

--- circle/index.php
+++ circle/index.php
@@ -1,3 +1,13 @@
 <?php  
 echo "songc";
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test</title>
+</head>
+<body>
+	<iframe width="100%" height="460px" src="http://vku.youku.com/live/share?id=11934" frameborder="0" allowfullscreen="true"></iframe>
+</body>
+</html>
